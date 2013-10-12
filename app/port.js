b1d5576f6a06e4de8939a68746bd547ac606c874
Luca Greco <luca.greco@alcacoop.it>
2013-10-12 22:28:25 +0200

    move app port initializer into port.js

--- app/port.js
+++ app/port.js
@@ -11,4 +11,22 @@ if (is_chrome) {
   Port = FirefoxPort;
 }
 
+Ember.Application.initializer({
+  name: "port",
+
+  initialize: function(container, application) {
+    container.register('port:main', application.Port);
+    container.lookup('port:main');
+  }
+});
+
+Ember.Application.initializer({
+  name: "injectPort",
+
+  initialize: function(container) {
+    container.typeInjection('controller', 'port', 'port:main');
+    container.typeInjection('route', 'port', 'port:main');
+  }
+});
+
 export default Port;

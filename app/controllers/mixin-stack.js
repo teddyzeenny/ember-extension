4e0a95d4061eec19ec8b2833cac3384a62f3443e
Teddy Zeenny <teddyzeenny@gmail.com>
2016-07-13 08:53:54 +0300

    Upgrade to Ember 2.0

--- app/controllers/mixin-stack.js
+++ app/controllers/mixin-stack.js
@@ -7,7 +7,7 @@ export default Controller.extend({
   trail: computed('model.[]', function() {
     let nested = this.get('model').slice(1);
     if (nested.length === 0) { return ""; }
-    return "." + nested.mapProperty('property').join(".");
+    return "." + nested.mapBy('property').join(".");
   }),
 
   isNested: computed('model.[]', function() {
